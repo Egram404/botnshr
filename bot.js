57591fef693efa0ed8cb3d8cd68e53138198fe9b
Egram404 <41884139+Egram404@users.noreply.github.com>
2019-01-25 19:57:11 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,32 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
+console.log("a7bk egram");
+ 
+ client.on("guildMemberRemove", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**
+سلام عليكم يا قلبي 
+لو سمحت ا ادخل السيرفر
+                               [ https://discord.gg/7vhfWTu ] 
+الدعووة خاصة لك ي قلبي ... [ ${member}  ]
+**`)
+}).catch(console.error)
+
+})
+ 
+client.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**
+سلام عليكم يا قلبي 
+
+لو سمحت ا ادخل السيرفر
+                               [ https://discord.gg/7vhfWTu ] 
+الدعووة خاصة لك ي قلبي ... [ ${member}  ]
+**`) 
+}).catch(console.error)
+})
+
+client.login(process.env.BOT_TOKEN);
