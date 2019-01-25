98e9588d35087b7eb983f35221cb2134952f5014
Egram404 <41884139+Egram404@users.noreply.github.com>
2019-01-25 19:40:29 +0300

    Add files via upload

---
+++ Sharing-BOT.js
@@ -0,0 +1,18 @@
+﻿const Discord = require('discord.js');
+const A7MD = new Discord.Client();
+
+console.log("BOT ONLINE");
+ 
+A7MD.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**
+السلام عليكم 
+
+فديتك...
+                               [https://discord.gg/26BQjzp] 
+الدعووة خاصة لك ... [ ${member}  ]
+**`) 
+}).catch(console.error)
+})
+A7MD.login('NDcxMjM5NjQ2ODc4MjM2Njgy.Djq5ew.ST2JcccJ25YjSeNqrLZbgGg-d-4');
